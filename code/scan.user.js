4de6118230707c74ef61c7d9a30d9624228df107
Wilhelm Mauch <80054014+riveruniversity@users.noreply.github.com>
2025-07-21 08:43:50 -0400

    Update scan.user.js

--- code/scan.user.js
+++ code/scan.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name			    Checkin Scan
 // @namespace	  	revival.com
-// @version		  	1.2.14
+// @version		  	1.2.15
 // @description		MP Checkin Suite extension
 // @author			  River Church
 // @match		    	https://mp.revival.com/checkin*
